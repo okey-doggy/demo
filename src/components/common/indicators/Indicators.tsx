578d46c508a41943bed7f1334223ca3173253cc8
rachel-park-dev <borapark13@gmail.com>
2023-10-21 23:27:28 +0900

    #fix dots error fixed

--- src/components/common/indicators/Indicators.tsx
+++ src/components/common/indicators/Indicators.tsx
@@ -1,23 +1,26 @@
-import {useState} from "react";
+import { useState } from "react";
 import styles from "./Indicators.module.scss";
 import Indicator from "./Indicator/Indicator.tsx";
 
-type TIndicators = {}
+interface IIndicators {}
 
-function Indicators({...props}: TIndicators) {
+function Indicators({ ...props }: IIndicators) {
   const [dots, setDots] = useState<number[]>(new Array(5).fill(0));
   const [activeDot, setActiveDot] = useState(0);
 
-  return (<div className={styles.container}>
-    {dots.map((_, index) => <div
-      key={index}
-      className={styles.dot}
-      onClick={() => setActiveDot(index)}
-    >
-      <Indicator/>
-    </div>)}
-  </div>);
+  return (
+    <div className={styles.container}>
+      {dots.map((_, index) => (
+        <div
+          key={index}
+          className={styles.dot}
+          onClick={() => setActiveDot(index)}
+        >
+          <Indicator />
+        </div>
+      ))}
+    </div>
+  );
 }
-
 
 export default Indicators;
