fafd2d717b08f157db5d49a1226a143d5f624a53
rachel-park-dev <borapark13@gmail.com>
2023-10-22 22:44:49 +0900

    #fix svg import error 수정.

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import svgr from "vite-plugin-svgr";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), svgr()],
+  resolve: {
+    alias: { "@": path.resolve(__dirname, "./src") },
+  },
+});
+
+// Install the @types/node package to fix the "Cannot find module 'path'" error
+// Run the following command in your terminal:
+// npm install --save-dev @types/node
