2693b4b542fe7e9d8bc5f4c6ce54fd1656c44d37
rachel-park-dev <borapark13@gmail.com>
2023-10-24 23:22:15 +0900

    #feat detail ui 추가.

--- src/components/pages/matching/select-training/detail/Detail.tsx
+++ src/components/pages/matching/select-training/detail/Detail.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styles from "./Detail.module.scss";
+import { trainings } from "@/constant/training";
+import Accordion from "./accordion/Accordion";
 
 function Detail() {
   //아코디언의 모음
@@ -9,7 +11,11 @@ function Detail() {
         <h1 className={styles.title}>훈련을 선택해주세요.</h1>
         <p className={styles.subtitle}>중복 선택 가능해요.</p>
       </div>
-      <section className={styles.section}></section>
+      <section className={styles.section}>
+        {trainings.map((training) =>
+          training.data.map((item) => <Accordion {...item} />)
+        )}
+      </section>
     </div>
   );
 }
--- src/components/pages/matching/select-training/detail/accordion/Accordion.tsx
+++ src/components/pages/matching/select-training/detail/accordion/Accordion.tsx
@@ -1,16 +1,19 @@
 import React from "react";
+import styles from "./Accordion.module.scss";
 
 interface IAccordion {
   id: number;
   title: string;
   description: string;
 }
-
-function Accordion({ ...props }: IAccordion) {
+function Accordion(item: IAccordion) {
   return (
-    <div>
-      <div></div>
-      <div></div>
+    <div className={styles.accordion}>
+      <div className={styles.title}>{item.title}</div>
+      <div className={styles.description}>
+        {item.description}
+        <div className={styles.dropdown}></div>
+      </div>
     </div>
   );
 }
