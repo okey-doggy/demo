8e705665d236bad0a906124b7224c43817ee44ce
rachel-park-dev <borapark13@gmail.com>
2023-10-29 15:45:46 +0900

    #modify etc page ui.

--- src/components/pages/matching/select-training/detail/etc/Etc.tsx
+++ src/components/pages/matching/select-training/detail/etc/Etc.tsx
@@ -4,6 +4,7 @@ import styles from "./Etc.module.scss";
 import Layout from "@/layout/Layout";
 import Header from "@/components/common/header/Header";
 import Indicators from "@/components/common/indicators/Indicators";
+import Underline from "@/assets/images/underline.svg";
 
 function Etc() {
   const navigate = useNavigate();
@@ -19,7 +20,14 @@ function Etc() {
             <h1 className={styles.title}>문제 행동에 대해 설명해주세요.</h1>
             <p className={styles.subtitle}>(선택 입력)</p>
           </div>
-          <section className={styles.section}></section>
+          <section className={styles.section}>
+            <input
+              type="text"
+              className={styles.textarea}
+              placeholder="예) 밥을 안먹어요"
+            />
+            <img src={Underline} alt="underline" className={styles.underline} />
+          </section>
         </div>
         <div className={styles.buttonWrapper}>
           <button className={styles.before} onClick={() => navigate(-1)}>
@@ -29,7 +37,7 @@ function Etc() {
             className={styles.complete}
             onClick={() => navigate(`/matching/select-place`)}
           >
-            선택 완료
+            다음
           </button>
         </div>
       </div>
