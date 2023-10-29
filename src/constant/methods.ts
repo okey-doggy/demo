d09f7591f317a58141d0bf28e9f775d4bc463155
rachel-park-dev <borapark13@gmail.com>
2023-10-29 16:06:50 +0900

    #feat resources added.

---
+++ src/constant/methods.ts
@@ -0,0 +1,13 @@
+export const methods = [
+  { id: 1, title: "방문 교육" },
+  { id: 2, title: "입소 훈련 1:1" },
+  { id: 3, title: "입소 훈련 1: 다수" },
+  { id: 4, title: "온라인 (화상 또는 전화)" },
+];
+
+export const onlineMethods = [
+  { id: 1, title: "온라인 화상 통화 (Google meet, zoom 등)" },
+  { id: 2, title: "유선 전화" },
+  { id: 3, title: "메신저 (카카오톡, 라인 등)" },
+  { id: 4, title: "기타" },
+];
--- src/constant/place.ts
+++
@@ -1,3 +0,0 @@
-export const places = [
-  { id: 1, title: "", description: "강아지와 함께 즐기는 카페" },
-];
